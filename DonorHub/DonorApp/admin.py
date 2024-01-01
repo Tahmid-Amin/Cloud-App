6cf17c11f2624885129986396a9658bef564d455
Tahmid Amin <tahmidamin26@gmail.com>
2024-01-01 14:48:15 +0000

    added the database model for the donors information

--- DonorHub/DonorApp/admin.py
+++ DonorHub/DonorApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import DonorInfo
 
 # Register your models here.
+admin.site.register(DonorInfo)
--- DonorHub/DonorApp/models.py
+++ DonorHub/DonorApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class DonorInfo(models.Model):
+    Firstname = models.CharField(max_length=100)
+    Surname = models.CharField(max_length=100)
+    DOB = models.DateField()
