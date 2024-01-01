18eadc9185b89210ed02d2e4e3c687ce71d7013f
Tahmid Amin <tahmidamin26@gmail.com>
2024-01-01 21:15:21 +0000

    basic template created, database display works

--- DonorHub/DonorApp/views.py
+++ DonorHub/DonorApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import DonorInfo
 # Create your views here.
-def home(request):
-    return HttpResponse('Hello world!')
+
+def donor_info(request):
+    donors = DonorInfo.objects.all()  # Retrieve all donors from the database
+    return render(request, 'donor_info.html', {'donors': donors})
+
+#def home(request):
+    #return HttpResponse('Hello world!')
