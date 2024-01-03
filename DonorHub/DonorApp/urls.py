860359d679b6f3209c550b5b18c845f6a6f9f44a
Tahmid Amin <tahmidamin26@gmail.com>
2024-01-03 21:39:26 +0000

    added query functions for donor data, and corresponding html pages

--- DonorHub/DonorApp/urls.py
+++ DonorHub/DonorApp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.donor_info, name = "home")
+    path("", views.donor_info, name = "home"),
+
+    path('search/', views.search, name = 'search'),
+    path('alter/', views.alter, name = 'alter'),
+    path('add/', views.add, name = 'add'),
+    path('remove/', views.remove, name = 'remove'),
 ]
--- DonorHub/DonorApp/views.py
+++ DonorHub/DonorApp/views.py
@@ -6,5 +6,18 @@ def donor_info(request):
     donors = DonorInfo.objects.all()  # Retrieve all donors from the database
     return render(request, 'donor_info.html', {'donors': donors})
 
+def search(request):
+    return render(request, 'search.html')
+
+def alter(request):
+    return render(request, 'alter.html')
+
+def add(request):
+    return render(request, 'add.html')
+
+def remove(request):
+    return render(request, 'remove.html')
+
+
 #def home(request):
     #return HttpResponse('Hello world!')
